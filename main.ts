cce1eef34d8ffb3d5d346236a10456d4fc2983b6
Rob Wilson <rob@rwilson.org.uk>
2022-02-03 09:20:04 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,9 @@
-serial.onDataReceived(serial.delimiters(Delimiters.Dollar), function () {
-    basic.showString(serial.readUntil(serial.delimiters(Delimiters.Dollar)))
-})
 input.onButtonPressed(Button.A, function () {
     serial.writeLine("A pressed")
 })
 input.onButtonPressed(Button.B, function () {
     serial.writeLine("B pressed")
+})
+serial.onDataReceived(serial.delimiters(Delimiters.CarriageReturn), function () {
+    basic.showString(serial.readUntil(serial.delimiters(Delimiters.CarriageReturn)))
 })
